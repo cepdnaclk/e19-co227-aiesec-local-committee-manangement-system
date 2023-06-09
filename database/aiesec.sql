b75d4622054f9f7089a9efa55d3a621b912559aa
Dinuka <rcavinash.d@gmail.com>
2023-06-09 16:03:39 +0530

    Member Tables created

--- database/aiesec.sql
+++ database/aiesec.sql
@@ -6,6 +6,12 @@ USE LC_KANDY
 CREATE TABLE IF NOT EXISTS MEMBERS_MAIN (
 
     Member_ID       INT(5) AUTO_INCREMENT PRIMARY KEY,
+
+    #login details
+    Personal_email  VARCHAR(255) NOT NULL,
+    Userpassword    VARCHAR(20),
+
+    #member details
     Full_Name       VARCHAR(100),
     Preferred_Name  VARCHAR(30),
     Function_id     INT(2),
@@ -13,15 +19,15 @@ CREATE TABLE IF NOT EXISTS MEMBERS_MAIN (
     Date_of_join    DATE,
     Position_id     INT(3),
     AIESEC_email    VARCHAR(255),
-    Personal_email  VARCHAR(255),
     Facebook_link   VARCHAR(255),
     LinkedIN_link   VARCHAR(255),
+    Instagram_link  VARCHAR(255),
     Contact_num     VARCHAR(12),
     Gender          CHAR(1),
     Birth_date      DATE,
     Faculty_id      CHAR(2),
     Batch           INT(2),
-    UniRegNo        VARCHAR(12),
+    UniRegNo        VARCHAR(12), #try to implement auto filling relevant fields when registering
     School_name     VARCHAR(50),
     Home_address    VARCHAR(100),
     District        VARCHAR(20),
@@ -44,13 +50,13 @@ CREATE TABLE IF NOT EXISTS A_FUNCTION (
 #Department id and name
 CREATE TABLE IF NOT EXISTS DEPARTMENT (
 
+    Dept_ID         INT(2) AUTO_INCREMENT PRIMARY Key,
     DName           VARCHAR(25),
-    Abbreviation    VARCHAR(10),
-    Dept_ID         INT(2) AUTO_INCREMENT PRIMARY Key
+    Abbreviation    VARCHAR(10)
 
 );
 
-#Valid pairs 
+#Valid pairs of Functions - Departments
 CREATE TABLE IF NOT EXISTS VALIDPAIRS (
 
     Func_ID INT(2),
@@ -60,3 +66,24 @@ CREATE TABLE IF NOT EXISTS VALIDPAIRS (
     FOREIGN KEY (Dept_ID) REFERENCES DEPARTMENTS(Dept_ID)
 
 );
+# new term details must be added manually at the start of every new term (hardcoding)
+CREATE TABLE IF NOT EXISTS TERMS (
+
+    Term_id                 INT(3) AUTO_INCREMENT PRIMARY KEY,
+    Term_name               VARCHAR(10), #Only Summer or winter
+    T_Year                  INT,    #T_Year and Term_name creates the full name of the term
+
+    Start_date              DATE,
+    End_date                DATE,
+    Newbie_Recruitment_date DATE
+
+);
+
+### TEAMS TABLE
+
+# iGV Data Tables
+
+/* CREATE TABLE IF NOT EXISTS IGV_PROJECTS (
+
+    Opportunity_id          INT(),
+) */
