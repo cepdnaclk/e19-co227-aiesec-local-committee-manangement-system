c93a609019af440b8cde3374fc2ff85d1d156be7
Manodya Senevirathne <e19366@eng.pdn.ac.lk>
2023-09-19 01:52:29 +0530

    Decoupled Email functions , changes in env, refctored ogv, email  scripts

--- server/src/route/email.js
+++ server/src/route/email.js
@@ -1,144 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path'); // Added for extracting filename from path
-const cron = require('node-cron');
-
-const nodemailer = require('nodemailer');
-const Mailgen = require('mailgen');
-require('dotenv').config();
-
-
-const { OAuth2Client } = require('google-auth-library');
 const jwt = require("jsonwebtoken");
 
-const REDIRECT_URL = 'http://localhost:8081/email/auth/callback';
-const oAuth2Client = new OAuth2Client(process.env.GMAIL_CLIENT_ID, process.env.GMAIL_CLIENT_SECRET, REDIRECT_URL);
-
-
 const { connection, execQuery } = require("../database/database");
+const { sendSystemEmail, sendUserEmail, replacePlaceholders, oAuth2Client } = require("../utils/email_functions");
 
 
-function send_email(reciver, cc, bcc, subject, body, attachments) {
+/*------------------ TEMPLATES ---------------------------------------*/
 
-    let config = {
-        service: 'gmail',
-        auth: {
-            user: process.env.GMAIL_APP_USER,
-            pass: process.env.GMAIL_APP_PASSWORD
-        }
-    };
-
-    let transporter = nodemailer.createTransport(config);
-
-    let ProcessedAttachments = attachments ? attachments.map(attachmentPath => {
-        return {
-            filename: path.basename(attachmentPath),
-            path: attachmentPath,
-            cid: path.basename(attachmentPath) // Extract filename for CID
-        };
-    }) : [];
-
-
-    let message = {
-        from: `no-reply AIESEC LCMS <${process.env.GMAIL_APP_USER}>`,
-        to: reciver,
-        cc: cc,
-        bcc: bcc,
-        subject: subject || 'system message from AIESEC lcms!',
-        html: body,
-        attachments: ProcessedAttachments
-    };
-
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(message, (error, info) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(info);
-            }
-        });
-    });
-
-}
-
-
-function replacePlaceholders(template, data) {
-    return template.replace(/\{\{(\w+)\}\}/g, function (match, placeholder) {
-        return data[placeholder] || '';
-    });
-}
-
-
-// for later usage
-let mailGenerator = new Mailgen({
-    theme: 'default',
-    product: {
-        // Appears in header & footer of e-mails
-        name: 'AIESEC local committee management system',
-        link: 'https://test.test/'
-    }
-});
-
-
-/*=======================REMINDERS=================================*/
-function fetchPreSignupMembers(callback) {
-    const query = 'SELECT * FROM OGVDetailsForSendReminders';
-
-    connection.query(query, (error, results) => {
-        if (error) throw error;
-        callback(results);
-    });
-}
-
-
-const sendReminders = (member_email, member_name, applicant_details) => {
-
-
-    let reminder_mail = "SELECT * FROM email_template WHERE name = 'reminder'";
-    let cc = null;
-    let bcc = null;
-
-    // Return the Promise from execQuery so it can be used in the Promise chain
-    return execQuery(reminder_mail)
-        .then((rows) => {
-            let emailData = rows[0];
-
-            template_data = {
-                member_name: member_name,
-                applicant_details: applicant_details
-            };
-
-            let emailBody = replacePlaceholders(emailData.body, template_data);
-
-            return send_email(member_email, cc, bcc, emailData.subject, emailBody, JSON.parse(emailData.attachments));
-        });
-
-};
-
-function callReminder() {
-
-    console.log("Sending Reminders to: ");
-
-    fetchPreSignupMembers(rows => {
-        rows.forEach(row => {
-
-            console.log(row.memberEmail);
-
-            let applicant_details = `<ul><li>id : ${row.id}</li><li>First Name : ${row.firstName}</li><li>Last Name : ${row.lastName}</li><li>Phone No : ${row.phone}</li><li>Campaign ID : ${row.campaignId}</li>`;
-
-            /*console.log("Reminder: ", row.member_email, row.member_name, applicant_details);*/
-            sendReminders(row.memberEmail, row.memberName, applicant_details);
-        
-        });
-    });
-};
-
-// sending reminders every 20 sec :: TODO: add suitable time period , formatting email
-/*cron.schedule('20 * * * * *', callReminder);*/
-
-/*=================================================================*/
-
-// routes
 
 // create email template
 router.post('/template/create', (req, res, next) => {
@@ -201,96 +70,25 @@ router.put('/template/:name', (req, res, next) => {
 });
 
 
-/*------------------EMAILS THROUGH SYSTEM---------------------------------------*/
+/*------------------ EMAILS THROUGH USERS ---------------------------------------*/
 
-// send a reminder (for testing)
-router.post('/sendReminders', (req, res, next) => {
-
-    sendReminders(req.body.receiver)
-        .then(() => {
-            res.status(200).json({ msg: 'Reminder sent' })
-        })
-        .catch((err) => {
-            next(err)
-        })
-
-});
-
-// for now testing only
-router.post('/send6weekChallengeMail', (req, res, next) => {
-    // below code only for testing 
-
-    // generate email
-    let emailBody = {
-        body: {
-            name: 'User',
-            intro: 'Reminder',
-            action: {
-                instructions: 'This is a test email :',
-                button: {
-                    color: 'green',
-                    text: 'Check',
-                    link: 'https://test.test'
-                }
-            }
-        }
-    };
-
-    // Using mailGenerator to generate the email body
-    let generatedEmailBody = mailGenerator.generate(emailBody);
-
-
-    // Process attachments
-    let attachments = req.body.attachments ? req.body.attachments : [];
-
-
-
-    send_email(req.body.email, req.body.cc, req.body.bcc, req.body.subject, generatedemailbody, attachments)
-        .then((info) => {
-            res.status(201).json({
-                msg: "email sent",
-                info: info.messageid,
-                preview: nodemailer.gettestmessageurl(info)
-            });
-        })
-        .catch((err) => {
-            next(err)
-        });
-
-
-});
-
-router.post('/sendApprovedMail', (req, res) => {
-
-});
-
-router.post('/sendESEMail', (req, res) => {
-
-});
-
-
-
-
-
-/*------------------EMAILS THROUGH USERS---------------------------------------*/
 
 //google authentication for user's to access data
 router.get('/auth', (req, res) => {
 
     try {
-
         const authUrl = oAuth2Client.generateAuthUrl({
             access_type: 'offline',
             scope: ['https://mail.google.com/', 'openid', 'email'],
         });
-        res.redirect(authUrl);
+        res.status(200).send(authUrl);
 
     } catch (err) {
         res.status(500).send("Google Authentication initialization error");
     }
 });
 
-
+// google redirects to this endpoint
 router.get('/auth/callback', async (req, res) => {
 
     try {
@@ -330,94 +128,19 @@ router.get('/auth/callback', async (req, res) => {
 
 
 router.get('/sendEmail', async (req, res, next) => {
-
     try {
-
-        const email = req.body.userEmail;  // TODO: Set this to the appropriate user email 
-
-        const rows = await execQuery(`SELECT * FROM user_gmail_data WHERE email = '${email}'`);
-
-        if (rows.length === 0) return res.status(404).send("User not found.");
-        const user = rows[0];
-
-
-        let access_token = user.accessToken;
-        let refresh_token = user.refreshToken;
-        let token_expiry = user.tokenExpiry;
-
-
-        if (Date.now() > token_expiry) {
-            const { credentials } = await oAuth2Client.refreshAccessToken();
-
-            access_token = credentials.access_token;
-            refresh_token = credentials.refresh_token;
-            token_expiry = credentials.expiry_date;
-
-            console.log("UPDATED credential :", credentials);
-
-            await execQuery(`UPDATE user_gmail_data SET accessToken = '${access_token}', tokenExpiry = '${token_expiry}' WHERE email = '${email}'`);
-
-        }
-
-        access_token = await new Promise((resolve, reject) => {
-            oAuth2Client.getAccessToken((err, token) => {
-                if (err) {
-                    reject("Failed to create access token :(");
-                }
-                resolve(token);
-            });
-        });
-
-        oAuth2Client.setCredentials({
-            refresh_token: refresh_token
-        });
-
-        let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-            auth: {
-                type: 'OAuth2',
-                user: email,
-                accessToken: access_token,
-                clientId: process.env.GMAIL_CLIENT_ID,
-                clientSecret: process.env.GMAIL_CLIENT_SECRET,
-                refreshToken: refresh_token
-            }
-        });
-
-
-        let ProcessedAttachments = req.body.attachments ? req.body.attachments.map(attachmentPath => {
-            return {
-                filename: path.basename(attachmentPath),
-                path: attachmentPath,
-                cid: path.basename(attachmentPath) // Extract filename for CID
-            };
-        }) : [];
-
-        let message = {
-            from: email,
-            to: req.body.receiver, 
-            cc: req.body.cc,
-            bcc: req.body.bcc,
-            subject: req.body.subject,
-            html: req.body.body,
-            attachments: ProcessedAttachments
-        };
-
-        transporter.sendMail(message, (error, info) => {
-            if (error) {
-                return res.status(500).send(error.message);
-            }
-            res.send('Email sent: ' + info.response);
-        });
-
-
+        const { userEmail, attachments, receiver, cc, bcc, subject, body } = req.body;
+        const response = await sendUserEmail(userEmail, attachments, receiver, cc, bcc, subject, body);
+        res.send('Email sent: ' + response);
     } catch (err) {
-        next(err);
+        if (err.message === "User Email is not Authenticated.") {
+            res.status(404).send(err.message);
+        } else {
+            next(err);
+        }
     }
 });
 
 
 
-module.exports = router;
+module.exports =  router;
--- server/src/route/ogv_applicant.js
+++ server/src/route/ogv_applicant.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Mailgen = require('mailgen');
+const cron = require('node-cron');
 
 const {
     requestBodyToFieldsAndValues,
@@ -7,15 +9,78 @@ const {
 } = require("../utils/parse");
 
 const { connection, execQuery, execQueryWithValues } = require("../database/database");
+const { sendSystemEmail, sendUserEmail, replacePlaceholders } = require("../utils/email_functions");
+
+///////////////utility functions
+
+// for later usage
+//let mailGenerator = new Mailgen({
+//    theme: 'default',
+//    product: {
+//        // Appears in header & footer of e-mails
+//        name: 'AIESEC local committee management system',
+//        link: 'https://test.test/'
+//    }
+//});
+
+function fetchPreSignupMembers(callback) {
+    const query = 'SELECT * FROM OGVDetailsForSendReminders';
+
+    connection.query(query, (error, results) => {
+        if (error) throw error;
+        callback(results);
+    });
+}
+
+const sendPreSignUpReminder = (member_email, member_name, applicant_details) => {
+
+    let reminder_mail = "SELECT * FROM email_template WHERE name = 'reminder'";
+    let cc = null;
+    let bcc = null;
+
+    return execQuery(reminder_mail)
+        .then((rows) => {
+            let emailData = rows[0];
+
+            // UPDATE THIS for placeholders
+            template_data = {
+                member_name: member_name,
+                applicant_details: applicant_details
+            };
+            let emailBody = replacePlaceholders(emailData.body, template_data);
+
+            return sendSystemEmail(member_email, cc, bcc, emailData.subject, emailBody, JSON.parse(emailData.attachments));
+        });
+
+};
+
+function sendPreSignUpReminders() {
+
+    fetchPreSignupMembers(rows => {
+        rows.forEach(row => {
+            console.log("Sending OGV Pre-Signup Reminder to: ",row.memberEmail);
+
+            let applicant_details = `<ul><li>id : ${row.id}</li><li>First Name : ${row.firstName}</li><li>Last Name : ${row.lastName}</li><li>Phone No : ${row.phone}</li><li>Campaign ID : ${row.campaignId}</li>`;
+            /*console.log("Reminder: ", row.member_email, row.member_name, applicant_details);*/
+            sendPreSignUpReminder(row.memberEmail, row.memberName, applicant_details);
+
+        });
+    });
+};
 
 
-//utility functions
 
 
 
 
-//routes
+// WARNING:be sure to not send large amount of emails in short peried
+// sending reminders every 20 sec:: TODO: add suitable time period
 
+/*cron.schedule('20 * * * * *', sendPreSignUpReminders);*/
+
+///////////////routes
+
+// get member details for 'select member' dropdown menu 
 router.get(`/members`, (req, res, next) => {
 
     execQuery(`SELECT id, preferred_name FROM member`)
@@ -53,8 +118,6 @@ router.get(`/:id`, (req, res, next) => {
         });
 
 });
-
-
 
 // new applicant
 router.post("/", (req, res, next) => {
@@ -112,7 +175,6 @@ router.put("/", (req, res, next) => {
     }
 });
 
-
 //delete application
 router.delete("/", (req, res, next) => {
     try {
@@ -132,9 +194,88 @@ router.delete("/", (req, res, next) => {
 
 
 
+//////////Emails
+
+// send a reminder (for testing)
+//router.post('/sendReminders', (req, res, next) => {
+
+//    sendReminders(req.body.receiver)
+//        .then(() => {
+//            res.status(200).json({ msg: 'Reminder sent' })
+//        })
+//        .catch((err) => {
+//            next(err)
+//        })
+
+//});
+
+
+// for now testing only
+router.post('/send6weekChallengeMail', (req, res, next) => {
+    // below code only for testing 
+
+    //// generate email
+    //let emailBody = {
+    //    body: {
+    //        name: 'User',
+    //        intro: 'Reminder',
+    //        action: {
+    //            instructions: 'This is a test email :',
+    //            button: {
+    //                color: 'green',
+    //                text: 'Check',
+    //                link: 'https://test.test'
+    //            }
+    //        }
+    //    }
+    //};
+
+    //// Using mailGenerator to generate the email body
+    //let generatedEmailBody = mailGenerator.generate(emailBody);
+
+
+    //// Process attachments
+    //let attachments = req.body.attachments ? req.body.attachments : [];
 
 
 
+    //sendSystemEmail(req.body.email, req.body.cc, req.body.bcc, req.body.subject, generatedemailbody, attachments)
+    //    .then((info) => {
+    //        res.status(201).json({
+    //            msg: "email sent",
+    //            info: info.messageid,
+    //            preview: nodemailer.gettestmessageurl(info)
+    //        });
+    //    })
+    //    .catch((err) => {
+    //        next(err)
+    //    });
+
+
+});
+
+router.post('/sendApprovedMail', (req, res) => {
+
+});
+
+router.get('/sendESEMail', (req, res) => {
+
+    // TODO ::
+    //      fetch email ESE mail template
+    //      replace placeholders data
+    //      send that mail data to client
+
+});
+
+router.post('/sendESEMail', (req, res) => {
+
+    // TODO ::
+    //      call sendUserEmail fn with req data
+    //      error handling
+    //      update member table
+    //      send back response
+
+});
 
 
 
